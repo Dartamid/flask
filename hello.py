1a5fae030567da7401b00996a9278ef46989bbcb
Dartamid <shabalin.dmm@yandex.ru>
2021-03-05 16:47:21 +0500

    Some fixes

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'У меня получилось!'
+    return 'У меня получилось!!!'
 
 
 if __name__ == '__main__':
-    print('Start working!')
+    print('Start working!!!')
     app.run(debug=True, host='0.0.0.0')
